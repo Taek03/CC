3a731afc843bab4600a47c030660cad896d438d2
Taek <agfd123454@naver.com>
2024-05-20 10:30:14 +0900

    최승호님, 이민준님 xml파일 합치기

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,15 +1,18 @@
 plugins {
     id("com.android.application")
+    id("com.google.gms.google-services")
 }
+
+apply(plugin = "com.google.gms.google-services")
 
 android {
     namespace = "com.example.collectconnect"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.example.collectconnect"
         minSdk = 28
-        targetSdk = 33
+        targetSdk = 34
         versionCode = 1
         versionName = "1.0"
 
@@ -32,10 +35,13 @@ android {
 }
 
 dependencies {
-
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.12.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
+    implementation("com.google.firebase:firebase-database:21.0.0")
+    implementation("com.google.firebase:firebase-auth:23.0.0")
+    implementation(platform("com.google.firebase:firebase-bom:33.0.0"))
+    implementation("com.google.firebase:firebase-analytics")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
